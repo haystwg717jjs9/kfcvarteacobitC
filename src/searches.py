edc8905300e24a7d601ff674fa0ea005d59f368b
Haris basure <anandranga038@outlook.com>
2024-07-30 06:35:13 +0530

    Update searches.py

--- src/searches.py
+++ src/searches.py
@@ -12,19 +12,16 @@ from selenium.webdriver.common.keys import Keys
 from src.browser import Browser
 from src.utils import Utils
 
-
 class Searches:
     def __init__(self, browser: Browser):
         self.browser = browser
         self.webdriver = browser.webdriver
 
     def getGoogleTrends(self, wordsCount: int) -> list:
-        # Function to retrieve Google Trends search terms
         searchTerms: list[str] = []
         i = 0
         while len(searchTerms) < wordsCount:
             i += 1
-            # Fetching daily trends from Google Trends API
             r = requests.get(
                 f'https://trends.google.com/trends/api/dailytrends?hl={self.browser.localeLang}&ed={(date.today() - timedelta(days=i)).strftime("%Y%m%d")}&geo={self.browser.localeGeo}&ns=15'
             )
@@ -42,7 +39,6 @@ class Searches:
         return searchTerms
 
     def getRelatedTerms(self, word: str) -> list:
-        # Function to retrieve related terms from Bing API
         try:
             r = requests.get(
                 f"https://api.bing.com/osjson.aspx?query={word}",
@@ -53,19 +49,19 @@ class Searches:
             return []
 
     def bingSearches(self, numberOfSearches: int, pointsCounter: int = 0):
-        # Function to perform Bing searches
         logging.info(
-            f"[BING] Starting {self.browser.browserType.capitalize()} Edge Bing searches..."
+            "[BING] "
+            + f"Starting {self.browser.browserType.capitalize()} Edge Bing searches...",
         )
 
         search_terms = self.getGoogleTrends(numberOfSearches)
         self.webdriver.get("https://bing.com")
-
+        
         i = 0
         attempt = 0
         for word in search_terms:
             i += 1
-            logging.info(f"[BING] {i}/{numberOfSearches}")
+            logging.info("[BING] " + f"{i}/{numberOfSearches}")
             points = self.bingSearch(word)
             if points <= pointsCounter:
                 relatedTerms = self.getRelatedTerms(word)[:0]
@@ -92,9 +88,6 @@ class Searches:
         return pointsCounter
 
     def bingSearch(self, word: str):
-        # Function to perform a single Bing search
-        i = 0
-
         while True:
             try:
                 self.browser.utils.waitUntilClickable(By.ID, "sb_form_q")
@@ -118,17 +111,6 @@ class Searches:
 
                 return self.browser.utils.getBingAccountPoints()
             except TimeoutException:
-                if i == 5:
-                    logging.info("[BING] " + "TIMED OUT GETTING NEW PROXY")
-                    self.webdriver.proxy = self.browser.giveMeProxy()
-                elif i == 10:
-                    logging.error(
-                        "[BING] "
-                        + "Cancelling mobile searches due to too many retries."
-                    )
-                    return self.browser.utils.getBingAccountPoints()
-                self.browser.utils.tryDismissAllMessages()
-                logging.error("[BING] " + "Timeout, retrying in 5~ seconds...")
-                time.sleep(Utils.randomSeconds(7, 15))
-                i += 1
+                logging.error("[BING] " + "Timeout, retrying in 5 seconds...")
+                time.sleep(5)
                 continue
